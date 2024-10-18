eff4182100464129f7952165aeab6df026aa28d6
Nora <43489162+NoraQuick@users.noreply.github.com>
2024-10-18 09:42:32 -0700

    Add files via upload

---
+++ Mutations.py
@@ -0,0 +1,17 @@
+# Mutations
+
+# Task
+# Read a given string, change the character at a given index and then print the modified string.
+
+def mutate_string(string, position, character):
+    l = list(string)
+    
+    l[position] = character
+    
+    return ''.join(l)
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
